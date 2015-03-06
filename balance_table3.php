d907e56df073c3673f3e743b7fc47b563e976742
Ramon <ramon.perez@datamininglab.com>
2015-03-05 21:59:16 -0500

    Testing href addition on balance_table3

--- balance_table3.php
+++ balance_table3.php
@@ -10,15 +10,25 @@
 
 <br>
 <form action="balance_table2.php" method="post">
+
 <td><b>
 <font face="Arial, Helvetica, sans-serif">Search by:</font>
 </b></td>
+
 Name <input type="text" name="name">
 Days <input type="number" name="days">
 <input type="submit">
+
 </form>
 </br>
+<?php	
+	$id=10;
+	$id_name='test';
+?>
 
+<a href="balance_table2.php?id=<?php echo $id;?>">
+  <?php echo $house_name;?>
+</a>
 
 
 </body>
