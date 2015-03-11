36ebc8baa3dedd093f96cd141462045adc725a40
Ramon <ramon.perez@datamininglab.com>
2015-03-11 13:31:24 -0400

    Simplified delete transactions page

--- delete_transaction.php
+++ delete_transaction.php
@@ -1,105 +1,13 @@
 <html>
 <head>
-<title>Edit the Following Transaction</title>
-<style>
-table, th, td {
-    border: 1px solid black;
-	background-color: #E6E6E6;
-}
-</style>
+<title>Delete Transaction</title>
+
 </head>
 <body>
 <?php
 	require_once 'config.php';
 	$search=$_GET['search'];
 	
-	$query="call get_balance (null, '$search')";
-	$result=mysql_query($query);
-	$num=mysql_numrows($result);
-
-	mysql_close();
-?>
-
-
-<table>
-<tr>
-<td><b>
-<font face="Arial, Helvetica, sans-serif">Date</font>
-</b></td>
-<td><b>
-<font face="Arial, Helvetica, sans-serif">Client Name</font>
-</b></td>
-<td><b>
-<font face="Arial, Helvetica, sans-serif">Company Name</font>
-</b></td>
-<td><b>
-<font face="Arial, Helvetica, sans-serif">Account</font>
-</b></td>
-<td><b>
-<font face="Arial, Helvetica, sans-serif">Category</font>
-</b></td>
-<td><b>
-<font face="Arial, Helvetica, sans-serif">Product</font>
-</b></td>
-<td><b>
-<font face="Arial, Helvetica, sans-serif">Notes</font>
-</b></td>
-<td><b>
-<font face="Arial, Helvetica, sans-serif">Transaction Amount</font>
-</b></td>
-<td><b>
-<font face="Arial, Helvetica, sans-serif">Balance</font>
-</b></td>
-</tr>
-
-<?php
-	$i=0;
-	while ($i < $num) {
-		$f1=mysql_result($result,$i,"Date");
-		$f2=mysql_result($result,$i,"Client Name");
-		$f3=mysql_result($result,$i,"Company Name");
-		$f4=mysql_result($result,$i,"Account");
-		$f5=mysql_result($result,$i,"Category");
-		$f6=mysql_result($result,$i,"Product");
-		$f7=mysql_result($result,$i,"Notes");
-		$f8=mysql_result($result,$i,"Transaction Amount");
-		$f9=mysql_result($result,$i,"Balance");
-?>
-
-<tr>
-<td>
-<font face="Arial, Helvetica, sans-serif"><?php echo $f1; ?></font>
-</td>
-<td>
-<font face="Arial, Helvetica, sans-serif"><?php echo $f2; ?></font>
-</td>
-<td>
-<font face="Arial, Helvetica, sans-serif"><?php echo $f3; ?></font>
-</td>
-<td>
-<font face="Arial, Helvetica, sans-serif"><?php echo $f4; ?></font>
-</td>
-<td>
-<font face="Arial, Helvetica, sans-serif"><?php echo $f5; ?></font>
-</td>
-<td>
-<font face="Arial, Helvetica, sans-serif"><?php echo $f6; ?></font>
-</td>
-<td>
-<font face="Arial, Helvetica, sans-serif"><?php echo $f7; ?></font>
-</td>
-<td>
-<font face="Arial, Helvetica, sans-serif"><?php echo $f8; ?></font>
-</td>
-<td>
-<font face="Arial, Helvetica, sans-serif"><?php echo $f9; ?></font>
-</td>
-</tr>
-
-<?php	$i++;}?>
-</table>
-
-<?php
 	$sql = "DELETE FROM transactions WHERE transID='$search' ";
 	
 	mysql_select_db($database);
