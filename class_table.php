0a69ee1b9abee0e5dd4fe9ce66abacf0dfbc2eda
Ramon <ramon.perez@datamininglab.com>
2015-03-20 00:06:31 -0400

    Update class table

--- class_table.php
+++ class_table.php
@@ -29,36 +29,29 @@
 
 <?php
 	require_once 'config.php';
-	$query1="select distinct first_name from clients order by first_name";
+	$query1="select distinct prod_name from products order by prod_name";
 	$result1=mysql_query($query1);
 	$num1=mysql_numrows($result1);
 	
-	$query2="select distinct last_name from clients order by last_name";
+	$query2="select distinct company_name from companies order by company_name";
 	$result2=mysql_query($query2);
 	$num2=mysql_numrows($result2);
 	
-	$query3="select distinct company_name from companies order by company_name";
-	$result3=mysql_query($query3);
-	$num3=mysql_numrows($result3);	
-	
-	$query="select * from clients order by first_name";
+	$query="call class_list(null, null)";
 	$result=mysql_query($query);
 	$num=mysql_numrows($result);
 	mysql_close();
 ?>
 
-<h1>Client List</h1>
+<h1>Class List</h1>
 <div class="datagrid">
 <table>
 <thead>
 <tr>
-<th>Name</th>
-<th>Street</th>
-<th>City</th>
-<th>State</th>
-<th>Zip</th>
-<th>Email</th>
-<th>Phone</th>
+<th>Class Name</th>
+<th>Meeting Days</th>
+<th>Start Date</th>
+<th>End Date</th>
 <th>Company</th>
 <th> </th>
 </tr>
@@ -70,33 +63,25 @@ if(isset($_POST['add'])){
 	$conn = mysql_connect($host_name,$username,$password);
 
 	if(! get_magic_quotes_gpc() ){
-		$first_name = addslashes ($_POST['first_name']);
-		$last_name = addslashes ($_POST['last_name']);
-		$street = addslashes ($_POST['street']);
-		$city = addslashes ($_POST['city']);
-		$state = addslashes ($_POST['state']);
-		$zip = addslashes ($_POST['zip']);
-		$email = addslashes ($_POST['email']);
-		$phone = addslashes ($_POST['phone']);
-		$company = addslashes ($_POST['company']);
+		$c_name = addslashes ($_POST['c_name']);
+		$p_name = addslashes ($_POST['p_name']);
+		$start_date = addslashes ($_POST['start_date']);
+		$end_date = addslashes ($_POST['end_date']);
+		$day_of_week = addslashes ($_POST['day_of_week']);
 	}
 	else{
-		$first_name = $_POST['first_name'];
-		$last_name = $_POST['last_name'];
-		$street = $_POST['street'];
-		$city = $_POST['city'];
-		$state = $_POST['state'];
-		$zip = $_POST['zip'];
-		$email = $_POST['email'];
-		$phone = $_POST['phone'];
-		$company = $_POST['company'];
+		$c_name = $_POST['c_name'];
+		$p_name = $_POST['p_name'];
+		$start_date = $_POST['start_date'];
+		$end_date = $_POST['end_date'];
+		$day_of_week = $_POST['day_of_week'];
 	}
 	
 	if ($zip==''){
 		$zip=00000;
 	}
-	$sql = "CALL mod_client ".
-       "('$first_name', '$last_name', '$street', '$city', '$state', '$zip', '$email', '$phone', '$company') ";
+	$sql = "CALL add_class ('$c_name', '$p_name', '$start_date', '$end_date', '$day_of_week') ";
+	
 	mysql_select_db($database);
 	$retval = mysql_query( $sql, $conn );
 
@@ -120,8 +105,8 @@ else
 <tr>
 
 <td>
-<input name='c_first_name' list="first" id="c_first_name">
-<datalist id="first">
+<input name='p_name' list="product" id="p_name">
+<datalist id="product">
 <?php
 	$i=0;
 	while ($i < $num1) {
@@ -129,99 +114,36 @@ else
 <option value="<?php echo $f1; ?>"><?php echo $f1; ?></option>
 <?php	$i++;}?>
 </datalist> 
-
-<input name='c_last_name' list="last" id="c_last_name">
-<datalist id="last">
-<?php
-	$i=0;
-	while ($i < $num2) {
-		$f2=mysql_result($result2,$i,"last_name");?>
-<option value="<?php echo $f2; ?>"><?php echo $f2; ?></option>
-<?php	$i++;}?>
-</datalist> 
 </td>
-
-<td><input name='street' type="text" id="street"></td>
-
-<td><input name='city' type="text" id="city"></td>
-
-<td>
-<input name='state' list="States" id="state">
-<datalist id="States">
-  <option value="AL">
-  <option value="AK">
-  <option value="AZ">
-  <option value="AR">
-  <option value="CA">
-  <option value="CO">
-  <option value="CT">
-  <option value="DC">
-  <option value="DE">
-  <option value="FL">
-  <option value="GA">
-  <option value="HI">
-  <option value="ID">
-  <option value="IL">
-  <option value="IN">
-  <option value="IA">
-  <option value="KS">
-  <option value="KY">
-  <option value="LA">
-  <option value="ME">
-  <option value="MD">
-  <option value="MA">
-  <option value="MI">
-  <option value="MN">
-  <option value="MS">
-  <option value="MO">
-  <option value="MT">
-  <option value="NE">
-  <option value="NV">
-  <option value="NH">
-  <option value="NJ">
-  <option value="NM">
-  <option value="NY">
-  <option value="NC">
-  <option value="ND">
-  <option value="OH">
-  <option value="OK">
-  <option value="OR">
-  <option value="PA">
-  <option value="PR">
-  <option value="RI">
-  <option value="SC">
-  <option value="SD">
-  <option value="TN">
-  <option value="TX">
-  <option value="UT">
-  <option value="VT">
-  <option value="VA">
-  <option value="WA">
-  <option value="WV">
-  <option value="WI">
-  <option value="WY">
-</datalist> 
-</td>
-
-<td><input name='zip' type="number" id="zip"></td>
-
-<td><input name='email' type="text" id="email"></td>
-
-<td><input name='phone' type="text" id="phone"></td>
 
 <td>
 <input name='c_name' list="comp" id="c_name">
 <datalist id="comp">
 <?php
 	$i=0;
-	while ($i < $num3) {
-		$f3=mysql_result($result3,$i,"company_name");?>
-<option value="<?php echo $f3; ?>"><?php echo $f3; ?></option>
+	while ($i < $num2) {
+		$f2=mysql_result($result2,$i,"company_name");?>
+<option value="<?php echo $f2; ?>"><?php echo $f2; ?></option>
 <?php	$i++;}?>
 </datalist> 
 </td>
 
-<td><input name="add" type="submit" id="add" value="Add Client"></td>
+<td><input name='start_date' type="date" id="start_date"></td>
+
+<td><input name='end_date' type="date" id="end_date"></td>
+
+<td>
+<input name='day_of_week' list="days" id="day_of_week">
+<datalist id="days">
+  <option value="Sunday">
+  <option value="Monday">
+  <option value="Tuesday">
+  <option value="Wednesday">
+  <option value="Thursday">
+  <option value="Friday">
+  <option value="Saturday">
+</datalist> 
+</td>
 </tr>
 </form>
 <?php
@@ -232,16 +154,12 @@ else
 <?php
 	$i=0;
 	while ($i < $num) {
-		$f1=mysql_result($result,$i,"first_name");
-		$f2=mysql_result($result,$i,"last_name");
-		$f3=mysql_result($result,$i,"street");
-		$f4=mysql_result($result,$i,"city");
-		$f5=mysql_result($result,$i,"state");
-		$f6=mysql_result($result,$i,"zip");
-		$f7=mysql_result($result,$i,"email");
-		$f8=mysql_result($result,$i,"phone");
-		$f9=mysql_result($result1,$i,"company_name");
-		$f10=mysql_result($result,$i,"clientID");
+		$f1=mysql_result($result,$i,"prod_name");
+		$f2=mysql_result($result,$i,"day_name");
+		$f3=mysql_result($result,$i,"start_date");
+		$f4=mysql_result($result,$i,"end_date");
+		$f5=mysql_result($result,$i,"company_name");
+		$f6=mysql_result($result,$i,"classID");
 ?>
 
 
@@ -249,9 +167,10 @@ else
 <tr>
 
 <td>
-<a href="balance_table2.php?search=<?php echo $f1.' '.$f2;?>">
-  <?php echo $f1.' '.$f2;?>
-</a>
+<?php echo $f1; ?>
+</td>
+<td>
+<?php echo $f2; ?>
 </td>
 <td>
 <?php echo $f3; ?>
@@ -263,19 +182,7 @@ else
 <?php echo $f5; ?>
 </td>
 <td>
-<?php echo $f6; ?>
-</td>
-<td>
-<?php echo $f7; ?>
-</td>
-<td>
-<?php echo $f8; ?>
-</td>
-<td>
-<?php echo $f9; ?>
-</td>
-<td>
-<a href="edit_client.php?search=<?php echo $f10;?>">
+<a href="edit_class.php?search=<?php echo $f6;?>">
   <?php echo 'Edit';?>
 </a>
 </td>
