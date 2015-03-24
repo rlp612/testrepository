58be6193463d0dcd403c44c13002acd7920cd5e9
Ramon <ramon.perez@datamininglab.com>
2015-03-24 12:58:20 -0400

    Added images to index file

--- index.php
+++ index.php
@@ -1,29 +1,106 @@
 ﻿<!doctype html>
 
 <html lang="en">
+<head>
+
+<style type="text/css">
+  .centeredImage
+    {
+    text-align:center;
+    margin-top:0px;
+    margin-bottom:0px;
+    padding:0px;
+    }
+</style>
+<style>
+.datagrid table { border-collapse: collapse; text-align: left; width: auto; } 
+.datagrid {display: inline-block; font: normal 16px/150% Arial, Helvetica, sans-serif; background: #fff; 
+	overflow: hidden; border: 3px solid #006699; -webkit-border-radius: 9px; -moz-border-radius: 9px; 
+	border-radius: 9px; }
+.datagrid table td, 
+.datagrid table th { text-align:center; padding: 10px 9px; }
+.datagrid table thead th {background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #006699), 
+	color-stop(1, #00557F) );background:-moz-linear-gradient( center top, #006699 5%, #00557F 100% );
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#006699', endColorstr='#00557F');
+	background-color:#006699; color:#FFFFFF; font-size: 16px; font-weight: bold; } 
+.datagrid table thead th:first-child { border: none; }
+.datagrid table tbody td { color: #00557F; 
+	border-left: 1px solid #E1EEF4;font-size: 16px;font-weight: normal; }
+.datagrid table tbody .alt td { background: #E1EEf4; color: #00557F; }
+.datagrid table tbody td:first-child { border-left: none; }
+.datagrid table tbody tr:last-child td { border-bottom: none; }
+.datagrid table tbody input {width:100%;}
+</style>
+
+</head>
 
 <body>
 
+<p class="centeredImage"><a href="http://www.worklifespanish.com">
+<img src="worklifespanish.png" alt="image description" height="150" width="200">
+</a>
+</p>
+
 <br>
-	<a href="balance_table.php">Get Balance</a>
-</br>
 <br>
-	<a href="client_table.php">Client List</a>
-</br>
 <br>
-	<a href="company_table.php">Company List</a>
-</br>
 <br>
-	<a href="account_table.php">Account List</a>
-</br>
-<br>
-	<a href="category_table.php">Category List</a>
-</br>
-<br>
-	<a href="product_table.php">Product List</a>
-</br>
-<br>
-	<a href="class_table.php">Class List</a>
-</br>
+
+
+<div class="datagrid">
+<table>
+<thead>
+<tr>
+<th>Get Balance</th>
+<th>Client List</th>
+<th>Company List</th>
+<th>Account List</th>
+<th>Category List</th>
+<th>Product List</th>
+<th>Class List</th>
+<tr>
+</thead>
+
+<tbody>
+<tr>
+<td>
+<a href="balance_table.php">
+<img src="Transactions list.jpg" alt="Get Balance" width="100%" height="50%" border="0">
+</a>
+</td>
+<td>
+<a href="client_table.php">
+<img src="client list.jpg" alt="Client List" width="100%" height="50%" border="0">
+</a>
+</td>
+<td>
+<a href="company_table.php">
+<img src="company list.jpg" alt="Company List" width="75%" height="50%" border="0">
+</a>
+</td>
+<td>
+<a href="account_table.php">
+<img src="account list.png" alt="Account List" width="100%" height="50%" border="0">
+</a>
+</td>
+<td>
+<a href="category_table.php">
+<img src="category list.jpg" alt="Category List" width="75%" height="30%" border="0">
+</a>
+</td>
+<td>
+<a href="product_table.php">
+<img src="product list.jpg" alt="Product List" width="75%" height="50%" border="0">
+</a>
+</td>
+<td>
+<a href="class_table.php">
+<img src="class list.jpg" alt="Class List" width="100%" height="50%" border="0">
+</a>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
 </body>
 </html>
